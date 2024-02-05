346723702045cd91c7f280fecdf25b145360db67
Maks12435 <phenixxx1243@gmail.com>
2024-02-05 22:29:01 +0600

    hhh

--- app/src/main/java/com/example/easymath/MainActivity.java
+++ app/src/main/java/com/example/easymath/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.easymath;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void startProfileActivity(View v){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/easymath/ProfileActivity.java
+++ app/src/main/java/com/example/easymath/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.example.easymath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void GoBack(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
