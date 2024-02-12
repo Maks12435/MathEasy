bcd52b0fdb8124e42099ea1e710891b57c3381b6
Maks12435 <phenixxx1243@gmail.com>
2024-02-13 00:40:45 +0600

    Login SigUp pages

--- app/src/main/java/com/example/easymath/LoginActivity.java
+++ app/src/main/java/com/example/easymath/LoginActivity.java
@@ -1,14 +1,52 @@
 package com.example.easymath;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class LloginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText usernameEditText = findViewById(R.id.editTextUsername);
+        final EditText passwordEditText = findViewById(R.id.editTextPassword);
+        Button loginButton = findViewById(R.id.buttonRegister);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (isValidCredentials(username, password)) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // Добавьте здесь код для обработки неверных данных, например, вывод сообщения об ошибке
+                }
+            }
+        });
+
+        TextView textViewLink = findViewById(R.id.signup);
+
+        textViewLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean isValidCredentials(String username, String password) {
+        return username.equals("Maks") && password.equals("1234");
     }
 }
--- app/src/main/java/com/example/easymath/SignupActivity.java
+++ app/src/main/java/com/example/easymath/SignupActivity.java
@@ -2,7 +2,10 @@ package com.example.easymath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        TextView textViewLink = findViewById(R.id.login);
+
+        textViewLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
