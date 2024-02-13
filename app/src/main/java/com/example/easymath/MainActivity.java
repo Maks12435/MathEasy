80c1cddb1a8de0379e58ca233cb73c8bfc34ccdb
Ernar <khanapin65@gmail.com>
2024-02-14 02:39:59 +0600

    menu from me

--- app/src/main/java/com/example/easymath/MainActivity.java
+++ app/src/main/java/com/example/easymath/MainActivity.java
@@ -1,19 +1,55 @@
 package com.example.easymath;
 
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageButton buttonDrawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        buttonDrawerLayout = findViewById(R.id.buttonDrawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+
+        buttonDrawerLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_lessons) {
+                    Toast.makeText(MainActivity.this, "Menu clicked", Toast.LENGTH_SHORT).show();
+                }
+
+                drawerLayout.close();
+
+                return false;
+            }
+        });
     }
 
     public void startProfileActivity(View v){
