075f23e1f0141c3dd86039c004a9a2ad4ce09b85
Ernar <khanapin65@gmail.com>
2024-04-26 11:49:55 +0500

    topics completed

---
+++ app/src/main/java/com/example/easymath/Test7Activity.java
@@ -0,0 +1,14 @@
+package com.example.easymath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test7);
+    }
+}
